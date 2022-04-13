d532cf11a713f5478f7afc457d3431072a46d7a7
Caayu <henrycay@outlook.com>
2022-04-13 10:42:43 -0300

    Primeiro commit

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+const path = require('node:path')
+const { VueLoaderPlugin } = require('vue-loader')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: './src/main.js'
+}
